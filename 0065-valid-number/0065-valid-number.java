a68f4dfd10204ea11bc409877d857f765ba6c59b
divyanshi-2612 <161453596+divyanshi-2612@users.noreply.github.com>
2025-05-21 15:29:56 +0530

    Time: 10 ms (11.26%), Space: 46 MB (5.06%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isNumber(String s) 
+    {
+        String pattern = "^[+-]?((\\d+(\\.\\d*)?)|(\\.\\d+))([eE][+-]?\\d+)?$";
+
+          return s.matches(pattern);
+    }
+}
