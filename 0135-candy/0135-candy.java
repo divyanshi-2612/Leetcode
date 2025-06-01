19d61af86f339b6ffacf261674b23224dda09a98
divyanshi-2612 <161453596+divyanshi-2612@users.noreply.github.com>
2025-06-02 02:39:48 +0530

    Time: 2 ms (99.56%), Space: 45.7 MB (70.42%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) 
+    {
+        int n= ratings.length;
+        int [] candies = new int[n];
+
+        //leftpass
+        for (int i=1; i<n; i++)
+        {
+            if (ratings[i]>ratings[i-1])
+            {
+                candies[i] = candies[i-1]+1;
+            }
+        }
+
+        //rightpass
+        for(int i=n-2; i>=0; i--)
+        {
+            if (ratings[i]>ratings[i+1])
+            {
+                candies[i] = Math.max(candies[i] , candies[i+1]+1);
+            }
+        }
+        int ans=0;
+        for(int i:candies)
+        {
+            ans+= i+1;
+        }
+
+        return ans;
+    }
+}
